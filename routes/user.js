44a93242f2706e1d675674f6347cb9675d6d86b8
Ryan <ROliver@zghealth.com>
2016-03-09 11:12:42 -0500

    Added edit-profile route and functionality

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,6 @@ router.get('/login', function (req, res) {
 		return res.redirect('/');
 	}
 	res.render('accounts/login', { message: req.flash('loginMessage')});
-
 });
 
 router.post('/login', passport.authenticate('local-login', {
@@ -64,11 +63,31 @@ router.post("/signup", function (req, res, next) {
 });
 
 
-router.get('logout', function (req, res, next) {
+router.get('/logout', function (req, res, next) {
 	req.logout();
 	res.redirect("/");
 });
 
+router.get('/edit-profile', function (req, res, next) {
+	res.render('accounts/edit-profile', { message: req.flash('success')});
+});
+
+router.post('/edit-profile', function (req, res, next) {
+	User.findOne({ _id: req.user._id }, function (err, user) {
+
+		if (err) return next(err);
+
+		if (req.body.name) user.profile.name = req.body.name;
+		if (req.body.address) user.address = req.body.address;
+
+		user.save(function (err) {
+			if (err) return next(err);
+
+			req.flash('success', 'Successfully edited your profile!');
+			return res.redirect('/edit-profile');
+		});
+	});
+});
 
 module.exports = router;
 

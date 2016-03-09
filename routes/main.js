247f26bd085d0284cbb58214b452a83262e7dbf0
Ryan <ROliver@zghealth.com>
2016-03-09 17:12:24 -0500

    add individual product view

--- routes/main.js
+++ routes/main.js
@@ -30,6 +30,16 @@ router.get('/products/:id', function (req, res, next) {
 		});
 });
 
+router.get('/product/:id', function (req, res, next) {
+	Product.findById({ _id: req.params.id }, function (err, product) {
+		if (err) return next(err);
+
+		res.render('main/product', {
+			product: product
+		});
+	});
+});
+
 // populate allows the products.category.name call in category.ejs
 
 
